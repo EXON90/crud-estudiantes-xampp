be086874c2909e6ed168faf6d1ae91114c8fddcb
EXON90 <eduardo.torres9099@gmail.com>
2026-05-28 23:03:50 -0500

    arreglo de conexion de db con php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,8 @@
 <?php
-$conexion = mysqli_connect("db", "root", "root", "bd_estudiantes");
-  
+$host = PHP_OS_FAMILY === "Windows" ? "localhost" : "host.docker.internal";
+$conexion = mysqli_connect($host, "root", "", "bd_estudiantes", 3306);
+
 if (!$conexion) {
-    die("❌ Error de conexión: " . mysqli_connect_error());
+    die("Error de conexión: " . mysqli_connect_error());
 }
 ?>
